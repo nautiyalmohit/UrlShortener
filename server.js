3482d043abf642bd3d5ec2f1346aead66d96defe
nautiyalmohit <mohit8nautiyal@gmail.com>
2023-01-12 19:07:33 +0530

    Add route for redirection

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import connectDB from './config/db.js'
 import {nanoid} from 'nanoid'
 import {urlsRouter} from './routes/url.js';
 import mongoose from 'mongoose';
+import { redirectRouter } from './routes/redirect.js';
 
 const app = express();
 mongoose.set('strictQuery', true);
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', urlsRouter);
+app.use('/', redirectRouter);
 
 app.listen(PORT, () => {
     console.log("App is up")
